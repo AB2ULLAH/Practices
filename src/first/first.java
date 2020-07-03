5ff7a7a206becdf134b8edcdc7711882ce87b079
ABDULLAH <ABDULLAH@192.168.10.10>
2020-07-03 08:59:30 +0500

    intial commit

---
+++ src/first/first.java
@@ -0,0 +1,20 @@
+package first;
+
+public class first {
+	  public static void main(String args[]) {
+		   /* String firstName = "John";
+		    String lastName = "Doe";
+		    System.out.println(firstName + " FUCKING " + lastName);
+		  }*/
+	
+		  String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+		  System.out.println(cars[0]);
+
+	  }
+
+	
+		
+	
+
+
+}
