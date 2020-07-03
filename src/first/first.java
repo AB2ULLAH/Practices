edaab6374949014dd9dd94e41018ab502b299f08
ABDULLAH <ABDULLAH@192.168.10.10>
2020-07-03 09:03:37 +0500

    removing

--- src/first/first.java
+++ src/first/first.java
@@ -4,7 +4,7 @@ public class first {
 	  public static void main(String args[]) {
 		   /* String firstName = "John";
 		    String lastName = "Doe";
-		    System.out.println(firstName + " FUCKING " + lastName);
+		    System.out.println(firstName  + lastName);
 		  }*/
 	
 		  String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
