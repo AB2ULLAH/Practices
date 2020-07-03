aaeb1c1d255245ef14581b35f9b7fd06bb5627a9
ABDULLAH <ABDULLAH@192.168.10.10>
2020-07-03 09:05:07 +0500

    added

--- src/first/first.java
+++ src/first/first.java
@@ -4,7 +4,7 @@ public class first {
 	  public static void main(String args[]) {
 		   /* String firstName = "John";
 		    String lastName = "Doe";
-		    System.out.println(firstName  + lastName);
+		    System.out.println(firstName + "Bobby" + lastName);
 		  }*/
 	
 		  String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
